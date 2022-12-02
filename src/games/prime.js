efa2fb2193d5f77a3d8b9ab18b1df892291dc79f
moxa-rumin <romamuhino1@gmail.com>
2022-12-02 06:05:00 +0530

    fix step 9

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,11 +2,19 @@ const questions = Array.from({ length: 3 }, () => [Math.floor(Math.random() * 98
 const rightAnswers = [];
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 for (let i = 0; i < 3; i += 1) {
+  let flag = true;
   for (let j = 2; j <= Math.sqrt(questions[i]); j += 1) {
     if (questions[i] % j === 0) {
-      rightAnswers.push('no');
+      // eslint-disable-next-line no-const-assign
+      flag = false;
+      break;
     }
   }
-  rightAnswers.push('yes');
+  if (flag) {
+    rightAnswers.push('yes');
+  } else {
+    rightAnswers.push('no');
+  }
 }
+
 export { questions, rightAnswers, rules };
